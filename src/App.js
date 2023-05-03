a5e0468bba3ef302f53f43da88c50166dc585ab9
airises <isaevasai7@gmail.com>
2023-05-03 21:12:56 +0600

    layout version

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+import Header from "./components/headers/Header";
+import MealSummary from "./components/meal-summary/MealSummary";
+import Meal from "./components/meal/Meal";
+import { useState } from "react";
+
+function App() {
+  return (
+    <Apps>
+      <Header />
+      <MealSummary />
+      <Meal />
+    </Apps>
+  );
+}
+const Apps = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+export default App;
